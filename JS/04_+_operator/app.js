f45e0d3de613deb8c74ae1ce9dc57fce3902b729
Alin Grigorescu <alin.grig87@gmail.com>
2023-02-28 21:08:15 +0200

    Javascript exercises

---
+++ JS/04_+_operator/app.js
@@ -0,0 +1,7 @@
+console.log(7 + 3); // 10
+console.log('7' + '3'); // '73'
+console.log(7 + '3' + 4); // 77 113 '734'
+console.log('3' + '2' + 7); // '327'
+console.log(3 + 4 + '2' + 3 + 4); // '727'
+console.log('3' * '4' + '5' * '2'); // '1210'
+console.log('9' - 9);
