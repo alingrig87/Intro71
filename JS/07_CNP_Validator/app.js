a706b604453cd19d13842e6863188ac2c77034d3
Alin Grigorescu <alin.grig87@gmail.com>
2023-03-02 19:49:55 +0200

    Add input variables

---
+++ JS/07_CNP_Validator/app.js
@@ -0,0 +1,8 @@
+var cnpInput = document.getElementById('cnp');
+var dayInput = document.getElementById('day');
+var monthInput = document.getElementById('month');
+var yearInput = document.getElementById('year');
+
+function validateCNP() {
+	console.log(cnpInput, dayInput, monthInput, yearInput);
+}
