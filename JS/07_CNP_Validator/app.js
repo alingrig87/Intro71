675a54b731af1ddccd62154af8730b0ac368ec31
Alin Grigorescu <alin.grig87@gmail.com>
2023-03-02 21:08:00 +0200

    CNP Validator Finished

--- JS/07_CNP_Validator/app.js
+++ JS/07_CNP_Validator/app.js
@@ -8,7 +8,8 @@ var output = document.getElementById('output');
 
 function validateCNP() {
 	if (isCNPValid() && isDayValid() && isMonthValid() && isYearValid()) {
-		console.log('CNP VALID');
+		output.textContent = 'CNP VALID';
+		output.style.color = 'green';
 	} else {
 		output.textContent = errorMessage;
 		output.style.color = 'red';
@@ -17,19 +18,98 @@ function validateCNP() {
 
 function isCNPValid() {
 	var cnp = cnpInput.value;
+	var genderFromCNP = Number(cnp[0]);
 
 	if (cnp.length != 13) {
 		errorMessage = 'CNP-ul trebuie sa contina 13 cifre';
 		return false;
+	} else if (isNaN(Number(cnp))) {
+		errorMessage = 'CNP trebuie sa contina doar cifre';
+		return false;
+	} else if (isNaN(genderFromCNP) || genderFromCNP < 1 || genderFromCNP > 8) {
+		errorMessage = 'Prima cifra din cnp trebuie sa fie intre 1 si 8';
+		return false;
+	} else if (isDayMonthCombinationValid() == false) {
+		errorMessage = 'Combinatie zi luna invalida';
+		return false;
 	}
-	// CNP should be a number - CNP trebuie sa contina doar cifre
-	// isNaN(Number(cnp))
-	// verify if first char is between 1 and 8
-	// Prima cifra din cnp trebuie sa fie intre 1 si 8
+
+	return true;
 }
 
-function isDayValid() {}
+function isDayValid() {
+	var day = Number(dayInput.value);
+	var dayFromCNP = cnpInput.value.substring(5, 7);
+	// var dayFromCNP = cnpInput.value[5] + cnpInput.value[6];
 
-function isMonthValid() {}
+	if (isNaN(day)) {
+		errorMessage = 'Ziua trebuie sa contina doar cifre';
+		return false;
+	} else if (day < 1 || day > 31) {
+		errorMessage = 'Ziua trebuie sa fie intre 1 si 31';
+		return false;
+	}
+	if (day != dayFromCNP) {
+		errorMessage = 'Ziua introdusa nu corespunde cu cea din CNP';
+		return false;
+	}
 
-function isYearValid() {}
+	return true;
+}
+
+function isMonthValid() {
+	var month = Number(monthInput.value);
+	var monthFromCNP = cnpInput.value.substring(3, 5);
+	// var monthFromCNP = cnpInput.value[3] + cnpInput.value[4];
+
+	if (isNaN(month)) {
+		errorMessage = 'Luna trebuie sa contina doar cifre';
+		return false;
+	} else if (month < 1 || month > 12) {
+		errorMessage = 'Luna trebuie sa fie intre 1 si 12';
+		return false;
+	}
+	if (month != monthFromCNP) {
+		errorMessage = 'Luna introdusa nu corespunde cu cea din CNP';
+		return false;
+	}
+	return true;
+}
+
+function isYearValid() {
+	var year = Number(yearInput.value);
+	var yearFromCNP = cnpInput.value.substring(1, 3);
+	// var yearFromCNP = cnpInput.value[1] + cnpInput.value[2];
+
+	if (isNaN(year)) {
+		errorMessage = 'Luna trebuie sa contina doar cifre';
+		return false;
+	} else if (year < 1900 || year > 2023) {
+		errorMessage = 'Anul trebuie sa fie intre 1900 si 2023';
+		return false;
+	}
+	if (yearInput.value.substring(2, 4) != yearFromCNP) {
+		errorMessage = 'Anul introdusa nu corespunde cu cel din CNP';
+		return false;
+	}
+
+	return true;
+}
+
+function isDayMonthCombinationValid() {
+	var month = Number(cnpInput.value.substring(3, 5));
+	var day = Number(cnpInput.value.substring(5, 7));
+
+	if (day == 0 || day > 31) {
+		return false;
+	} else if (month == 2 && day > 28) {
+		return false;
+	} else if (
+		(month == 4 || month == 6 || month == 9 || month == 11) &&
+		day > 30
+	) {
+		return false;
+	}
+
+	return true;
+}
